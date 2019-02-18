b1622f037609f5acf3f0e0a70a07e448c0ab090d
Pedro Honda <pedro.koh@gmail.com>
2019-02-17 22:43:57 -0300

    2019-02-17 : Using GUI template from https://stackoverflow.com/questions/7546050

--- templates/graphic.py
+++ templates/graphic.py
@@ -1,23 +1,26 @@
-# Based on templates from: https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
-###########################################################################################################
-import tkinter as tk
-from tkinter import font  as tkfont
-from tkinter import ttk
+# Template from: https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+#################################################################################################
+import tkinter as tk                # python 3
+from tkinter import font  as tkfont # python 3
+from tkinter import ttk             # python 3
+#import Tkinter as tk     # python 2
+#import tkFont as tkfont  # python 2
 
-class AndroidLoggingGUI(tk.Tk):
+class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
         container = tk.Frame(self)
-        container.pack(side='top', fill='both', expand=True)
+        container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-        
-        self.centralize(550,550)
-        
+
         self.frames = {}
         for F in (StartPage, PageOne, PageTwo):
             page_name = F.__name__
@@ -29,21 +32,12 @@ class AndroidLoggingGUI(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.showFrame("StartPage")
+        self.show_frame("StartPage")
 
-    def centralize(self, width, height):
-        self.resizable(width=True, height=True)
-        ws = self.winfo_screenwidth()
-        hs = self.winfo_screenheight()
-        x = (ws/2) - (width/2)
-        y = (hs/2) - (height/2)
-        self.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
-    def showFrame(self, n):
-        frame = self.frames[n]
-        frame.update()
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
         frame.tkraise()
-
 
 
 class StartPage(tk.Frame):
@@ -55,9 +49,9 @@ class StartPage(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
 
         button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.showFrame("PageOne"))
+                            command=lambda: controller.show_frame("PageOne"))
         button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.showFrame("PageTwo"))
+                            command=lambda: controller.show_frame("PageTwo"))
         button1.pack()
         button2.pack()
 
@@ -70,7 +64,7 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text="This is page 1", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.showFrame("StartPage"))
+                           command=lambda: controller.show_frame("StartPage"))
         button.pack()
 
 
@@ -82,5 +76,10 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="This is page 2", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.showFrame("StartPage"))
+                           command=lambda: controller.show_frame("StartPage"))
         button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
