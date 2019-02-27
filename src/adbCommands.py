f980317740c2b05b6e1fecab86367e671af3321b
Pedro Honda <pedro.koh@gmail.com>
2019-02-26 23:11:12 -0300

    2019-02-26 : - Implementation of all (maybe) methods on adbCommands - Began a small scratch of the Main Frame of graphic

--- src/adbCommands.py
+++ src/adbCommands.py
@@ -8,12 +8,62 @@ class adbClass():
     def adbRoot(self):
         subprocess.call("adb -s " + self.serialno + " root")
 
+    def cleanLogging(self):
+        subprocess.call("adb -s " + self.serialno + " shell rm -rf /sdcard/Logging/*")
+
     def startVideo(self):
+        """ 
+            adb shell screenrecord /sdcard/Logging/video.mp4
+        """
         subprocess.call("adb -s " + self.serialno + " shell screenrecord /sdcard/Logging/video.mp4")
-    
+
+    def stopVideo(self):
+        """ 
+            adb shell ps -A | grep screenrecord
+            -- This gives some information about the process such as its PID, which can be used to kill the process with:
+            -- adb shell kill <PID>
+        """
+        psVideo = subprocess.getoutput("adb -s " + self.serialno + " shell ps -A | grep screenrecord").split()[1]
+        if psVideo:
+            subprocess.call("adb -s " + self.serialno + " shell kill " + psVideo)
+            return True
+        else:
+            return False
+        
     def startAPlog(self):
-        subprocess.call("adb -s " + self.serialno + " shell aplogcat -d -o /sdcard/Logging/ -n 1")
+        """ 
+            adb shell "logcat > /sdcard/Logging/APlogging.txt"
+        """
+        subprocess.call("adb -s " + self.serialno + " shell \"logcat > /sdcard/Logging/APlogging.txt\"")
+
+    def stopAPlog(self):
+        """ 
+            adb shell ps -A | grep logcat
+            -- This gives some information about the process such as its PID, which can be used to kill the process with:
+            -- adb shell kill <PID>
+        """
+        psLogcat = subprocess.getoutput("adb -s " + self.serialno + " shell ps -A | grep logcat").split()[1]
+        if psLogcat:
+            subprocess.call("adb -s " + self.serialno + " shell kill " + psLogcat)
+            return True
+        else:
+            return False
 
     def startTCPdump(self):
+        """ 
+            adb shell tcpdump -v -s 0 -i any -w /sdcard/Logging/packets.pcap
+        """
         subprocess.call("adb -s " + self.serialno + " shell tcpdump -v -s 0 -i any -w /sdcard/Logging/packets.pcap")
 
+    def stopTCPdump(self):
+        """ 
+            adb shell ps -A | grep tcpdump
+            -- This gives some information about the process such as its PID, which can be used to kill the process with:
+            -- adb shell kill <PID>
+        """
+        psTCP = subprocess.getoutput("adb -s " + self.serialno + " shell ps -A | grep tcpdump").split()[1]
+        if psTCP:
+            subprocess.call("adb -s " + self.serialno + " shell kill " + psTCP)
+            return True
+        else:
+            return False
--- src/graphic.py
+++ src/graphic.py
@@ -4,6 +4,9 @@ import tkinter as tk
 from tkinter import font  as tkfont
 from tkinter import ttk
 
+import subprocess
+import adbCommands
+
 class AndroidLoggingGUI(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -51,15 +54,43 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
+        self.updateADBdevices()
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.showFrame("PageOne"))
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.showFrame("PageTwo"))
-        button1.pack()
-        button2.pack()
+        # Label to Drop Down Menu for Android devices available
+        dropMenuDevicesText = ttk.Label(self, text="Choose an Android device:")
+        dropMenuDevicesText.grid(row=0, column=0, sticky="nsew")
+        # Drop Menu containing all devices attached
+        self.dropMenuDevicesSet = tk.StringVar()
+        self.dropMenuDevicesSet.set(self.devices[0])
+        self.dropMenuDevices = ttk.OptionMenu(self, self.dropMenuDevicesSet, self.devices[0], *self.devices, command=self.changeDevice)
+        self.dropMenuDevices.grid(row=0, column=1, sticky="nsew")
+        # Button to update current devices
+        dropMenuDevicesUpdate = ttk.Button(self, text="UPDATE", command=self.updateADBdevices)
+        dropMenuDevicesUpdate.grid(row=0, column=2, sticky="nsew")
+        
+
+    def updateADBdevices(self):
+        self.devices = []
+        self.android = ''
+        adbDevices = subprocess.getoutput("adb devices")
+        for d in adbDevices.split("\n"):
+            if "\tdevice" in d:
+                self.devices.append(d.split("\tdevice")[0])
+        if not self.devices:
+            self.devices.append("No Devices attached")
+        else:
+            self.android = self.devices[0]
+
+    def changeDevice(self, value):
+        if "No Devices attached" not in value:
+            self.android = value
+
+    def refreshDevices(self):
+        self.updateADBdevices()
+        menu = self.dropMenuDevices["menu"]
+        menu.delete(0, "end")
+        for d in self.devices:
+            menu.add_command(label=d, command=lambda value=d: self.dropMenuDevicesSet.set(value))
 
 
 class PageOne(tk.Frame):
